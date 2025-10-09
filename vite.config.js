ff21d8416cbdfb917cabcdf2af9d051184d5f332
TrumpetGirl <maria_sel_94@hotmail.com>
2025-10-09 13:18:45 +0200

    Publicando web más intentos

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio_Fernando/',
-  plugins: [react()]
+  base: 'https://trumpetgirl.github.io/portfolio_Fernando/',
+  plugins: [react()],
 })
